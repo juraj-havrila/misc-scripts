58f346cc0fcb060c78c6c019bafdea750e1510d2
Juraj Havrila <havrila.juraj@gmail.com>
2017-12-13 11:25:03 +0100

    adjusted comments

--- RCS.check.in.the.sh
+++ RCS.check.in.the.sh
@@ -3,13 +3,13 @@
 #Juraj Havrila, 12.12.2017, based on not working script from A.Neufeld (23.7.2015)
 
 for file in `cat /var/log/vicCheck | grep "not checked in" | sed 's#^Error: ##g' | sed 's# not checked in$##g'` ; do
-    old_dir=`pwd`
         my_file=`basename "${file}"`
         my_dir=`dirname "${file}"`
         my_RCS_link="${my_dir}/RCS"
         my_conf_dir="/conf${my_dir}"
         my_conf_file="/conf${my_dir}/${my_file},v"
 
+    if [ -e ${file} ]; then
         mkdir -p ${my_conf_dir}
 
         if [ -d ${my_RCS_link} -a ! -L ${my_RCS_link} ]; then
@@ -22,10 +22,11 @@ for file in `cat /var/log/vicCheck | grep "not checked in" | sed 's#^Error: ##g'
 
         if [ -e  ${my_conf_file} ]; then
             rcs -l "${my_dir}/${my_file}"
-            ci -m'WT-1015 - automatic RCS check in' -u "${my_dir}/${my_file}"
+            ci -m'WT-1015 - automatic RCS check in (Bereinigung der nicht eingecheckten Edits)' -u "${my_dir}/${my_file}"
         else
-            rcs -i -t-'WT-1015 - automatic RCS check in' "${my_dir}/${my_file}"
+            rcs -i -t-'WT-1015 - automatic RCS check in (Bereinigung der nicht eingecheckten Edits)' "${my_dir}/${my_file}"
             rcs -l "${my_dir}/${my_file}"
-            ci -m'WT-1015 - automatic RCS check in' -u "${my_dir}/${my_file}"
+            ci -m'WT-1015 - automatic RCS check in (Bereinigung der nicht eingecheckten Edits)' -u "${my_dir}/${my_file}"
         fi
+    fi
 done
